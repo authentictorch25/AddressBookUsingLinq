1c1bdf52500b6cb9909fba9fde40243997fdbaf4
authentictorch25 <64377971+authentictorch25@users.noreply.github.com>
2020-11-09 23:03:34 +0530

    [Akash] Added program to create Contact

---
+++ AddressBookModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBookUsingLinq
+{
+    public class AddressBookModel
+    {
+        public string firstName { get; set; }
+        public string secondName { get; set; }
+        public string address { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public long zip { get; set; }
+        public long phoneNumber { get; set; }
+        public string email { get; set; }
+        public string contactType { get; set; }
+        public string addressBookName { get; set; }
+    }
+}
